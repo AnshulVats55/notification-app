2dad80233845176fe215e7e3f7352dee4fd61b6c
AnshulVats55 <anshul.vats@fiftyfivetech.io>
2024-05-24 22:36:23 +0530

    feat: This commit changes the app_id and api_key for OneSignal

--- db.js
+++ db.js
@@ -5,20 +5,20 @@ dotenv.config();
 export const sendNotification = (request, response) => {
   const options = {
     method: "POST",
-    url: "https://onesignal.com/api/v1/notifications?",
-    params: { app_id: "9a37f401-19dd-4edc-afe7-a666b14c6ff6" },
+    url: "https://onesignal.com/api/v1/notifications",
+    params: { app_id: "e4a9b913-d9a6-44b5-aac9-4ae022e599d4" },
     headers: {
       cookie:
         "__cf_bm=zHfLDveHas5NSFQei9hTeVyXtIES8x2oYPG91NHpbPE-1716543901-1.0.1.1-io3CgWaGg.5lW1wHYlC0.tRUx0xzBJvG.4PEYe0OtnoSq06fODda8UUjghLwm_TKjN_DW5c2rPPGb4P_Z7rswg",
       "Content-Type": "application/json",
       "User-Agent": "insomnia/9.1.1",
-      Authorization: "Basic Mzg0NTY3MDktMDhlNy00YTk3LThjZjItMWU1MGVjMzE4ZTk0",
+      Authorization: "Basic MmEwNDc3ZWEtYjJhMC00MjBhLTk4ZjctMzY2MzM5MmM5N2Iw",
     },
     data: {
       included_segments: ["Subscribed Users"],
-      contents: { en: "From Node" },
+      contents: { en: "Notification from NodeJS" },
       include_aliases: {
-        onesignal_id: ["2d4e9a8f-e53a-4e01-8764-3ad5bbb4cea9"],
+        onesignal_id: ["77feae12-024f-482c-94b1-463393662d8d"],
       },
       target_channel: "push",
     },
