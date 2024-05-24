73f8506330f59fdc4a56a6b6c8c4b63f4ae306bc
AnshulVats55 <anshul.vats@fiftyfivetech.io>
2024-05-24 22:08:04 +0530

    dummy

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(cors());
 app.listen(4000, (req, res) => {
   console.log("server up");
-  res.send("Server Up")
 });
-
+app.get("/", (req, res)=>res.send("Server running"))
 app.get("/api/notification", sendNotification);
