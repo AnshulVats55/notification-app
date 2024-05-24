6723124fdb8bf3fe720b6c21f5d1aa89f5025798
AnshulVats55 <anshul.vats@fiftyfivetech.io>
2024-05-24 23:12:53 +0530

    feat: This commit changes the app_id and api_key for OneSignal

--- db.js
+++ db.js
@@ -8,8 +8,8 @@ export const sendNotification = (request, response) => {
     url: "https://onesignal.com/api/v1/notifications",
     params: { app_id: "e4a9b913-d9a6-44b5-aac9-4ae022e599d4" },
     headers: {
-      // cookie:
-      //   "__cf_bm=zHfLDveHas5NSFQei9hTeVyXtIES8x2oYPG91NHpbPE-1716543901-1.0.1.1-io3CgWaGg.5lW1wHYlC0.tRUx0xzBJvG.4PEYe0OtnoSq06fODda8UUjghLwm_TKjN_DW5c2rPPGb4P_Z7rswg",
+      cookie:
+        "__cf_bm=zHfLDveHas5NSFQei9hTeVyXtIES8x2oYPG91NHpbPE-1716543901-1.0.1.1-io3CgWaGg.5lW1wHYlC0.tRUx0xzBJvG.4PEYe0OtnoSq06fODda8UUjghLwm_TKjN_DW5c2rPPGb4P_Z7rswg",
       "Content-Type": "application/json",
       // "User-Agent": "insomnia/9.1.1",
       Authorization: "Basic MmEwNDc3ZWEtYjJhMC00MjBhLTk4ZjctMzY2MzM5MmM5N2Iw",
